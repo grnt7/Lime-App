a80a0dc4ee113a443200f14f27c6ce8b0b5ee309
David <grantclan.7@gmail.com>
2025-07-02 11:09:52 +0100

    map added

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,18 @@
 import { Stack, Link } from 'expo-router';
-
+import { View } from 'react-native'; // Add View and StyleSheet import
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
+import Map from "~/components/Map";
 import { ScreenContent } from '~/components/ScreenContent';
+
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+       <View style={{ flex: 1 }}> {/* Give the parent explicit flex:1 */}
+          <Map/>
+      </View>
     </>
   );
 }
